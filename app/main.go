bb594f6f040cf2179e8d5b62bcc0dd1c0be49fde
Kate Sychenko <busy.sychenko@gmail.com>
2024-08-13 22:18:01 +0400

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -2,47 +2,54 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
-	// "net"
+	"log"
+	"net"
 )
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
-	// udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
-	// if err != nil {
-	// 	fmt.Println("Failed to resolve UDP address:", err)
-	// 	return
-	// }
-	//
-	// udpConn, err := net.ListenUDP("udp", udpAddr)
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to address:", err)
-	// 	return
-	// }
-	// defer udpConn.Close()
-	//
-	// buf := make([]byte, 512)
-	//
-	// for {
-	// 	size, source, err := udpConn.ReadFromUDP(buf)
-	// 	if err != nil {
-	// 		fmt.Println("Error receiving data:", err)
-	// 		break
-	// 	}
-	//
-	// 	receivedData := string(buf[:size])
-	// 	fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
-	//
-	// 	// Create an empty response
-	// 	response := []byte{}
-	//
-	// 	_, err = udpConn.WriteToUDP(response, source)
-	// 	if err != nil {
-	// 		fmt.Println("Failed to send response:", err)
-	// 	}
-	// }
+	//Uncomment this block to pass the first stage
+
+	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
+	if err != nil {
+		fmt.Println("Failed to resolve UDP address:", err)
+		return
+	}
+
+	udpConn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		fmt.Println("Failed to bind to address:", err)
+		return
+	}
+	defer func(udpConn *net.UDPConn) {
+		err := udpConn.Close()
+		if err != nil {
+			//fmt.Fprint(os.Stderr, "Failed to close to UDP connection:", err)
+			log.Fatalf("Failed to close to UDP connection: %s", err)
+
+		}
+	}(udpConn)
+
+	buf := make([]byte, 512)
+
+	for {
+		size, source, err := udpConn.ReadFromUDP(buf)
+		if err != nil {
+			fmt.Println("Error receiving data:", err)
+			break
+		}
+
+		receivedData := string(buf[:size])
+		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
+
+		// Create an empty response
+		response := []byte{}
+
+		_, err = udpConn.WriteToUDP(response, source)
+		if err != nil {
+			fmt.Println("Failed to send response:", err)
+		}
+	}
 }
